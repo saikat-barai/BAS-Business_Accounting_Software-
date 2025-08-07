7d7ad1e3e5a5413ac29e006b06b41c47a2714fad
Saikat Barai <saikatbarai299@.com>
2025-08-07 07:39:19 +0600

    total income & expense & profit/loss shown on dashboard

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -3,11 +3,24 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Account;
+use App\Models\Expense;
+use App\Models\Payment;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function dashboard(){
-        return view('backend.dashboard.bashboard');
+    public function dashboard()
+    {
+        $accountIds = Account::whereIn('type', ['bank', 'cash'])->pluck('id');
+        $totalIncome = Payment::whereIn('account_id', $accountIds)->sum('amount');
+
+        $totalExpense = Expense::sum('amount');
+
+        $profitOrLoss = $totalIncome - $totalExpense;
+        
+        $bankCashAccounts = Account::whereIn('type', ['bank', 'cash'])->sum('current_balance');
+
+        return view('backend.dashboard.bashboard', compact('totalIncome', 'totalExpense', 'profitOrLoss', 'bankCashAccounts'));
     }
 }
